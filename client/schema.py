96a64d71c7c67f60ec9d90b23dce3796c52e2c1d
mathbrahian <mathbrahian@gmail.com>
2022-12-05 21:58:08 -0500

    Mutation and Query of client with graphene

--- client/schema.py
+++ client/schema.py
@@ -1,14 +1,41 @@
 import graphene
 from graphene_django import DjangoObjectType
-from client.models import Client as ClientModel
+from client.models import Client as Client
 
-class Client(DjangoObjectType):
+class ClientType(DjangoObjectType):
     class Meta:
-        model = ClientModel
+        model = Client
 
-class Query(graphene.ObjectType):
-    clients = graphene.List(Client)
+class UpdateClientInput(graphene.InputObjectType):
+    name = graphene.String(required=False)
+    email = graphene.String(required=False)
+    direction = graphene.String(required=False)
+    phone_number = graphene.String(required=False)
+
+class CreateClientInput(graphene.InputObjectType):
+    name = graphene.String(required=True)
+    email = graphene.String(required=True)
+    direction = graphene.String(required=True)
+    phone_number = graphene.String(required=True)
+
+class CreateClient(graphene.Mutation):
+    class Arguments:
+        client_data = ClientInput(required=True)
+
+    client = graphene.Field(ClientType)
+
+    def mutate(root, info, client_data=None):
+        client = Client(**client_data)
+        client.save()
+        return CreateClient(client=client)
+
+class Mutation(graphene.ObjectType):
+    create_client = CreateClient.Field()
+
+class Query(graphene.ObjectType): 
+    client = graphene.Field(ClientType)
+    clients = graphene.List(ClientType)
     def resolve_clients(self, info):
-        return ClientModel.objects.all()
+        return Client.objects.all()
 
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query, mutation=Mutation)
